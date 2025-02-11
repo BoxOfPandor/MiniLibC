95a8300a8c436609082d87ff9e69867df3f36ec3
Thibault <thibault.pouch@epitech.eu>
2025-02-11 20:54:28 +0100

    [feat] strlen : add strlen implementation in assembly

---
+++ src/strlen.s
@@ -0,0 +1,21 @@
+;
+; EPITECH PROJECT, 2024
+; B-ASM-400-LIL-4-1-asmminilibc-thibault.pouch
+; File description:
+; strlen.s
+;
+
+section .text
+    global strlen
+
+strlen:
+    xor rax, rax
+
+.loop:
+    cmp byte [rdi + rax], 0
+    je .done
+    inc rax
+    jmp .loop
+
+.done:
+    ret
