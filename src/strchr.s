4afbf80694703660a2b377440f45a6ab2c8d410e
Thibault <thibault.pouch@epitech.eu>
2025-02-12 09:11:01 +0100

    [feat] strchr : add strchr implementation in assembly

---
+++ src/strchr.s
@@ -0,0 +1,31 @@
+;
+; EPITECH PROJECT, 2024
+; B-ASM-400-LIL-4-1-asmminilibc-thibault.pouch
+; File description:
+; strchr.s
+;
+
+section .text
+    global asm_strchr
+
+asm_strchr:
+    xor rax, rax
+    mov rcx, rdi
+    mov al, sil
+
+.loop:
+    mov dl, [rcx]
+    cmp dl, al
+    je .found
+    cmp dl, 0
+    je .not_found
+    inc rcx
+    jmp .loop
+
+.found:
+    mov rax, rcx
+    ret
+
+.not_found:
+    xor rax, rax
+    ret
